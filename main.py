a1d02a4d977023e4b623e2f10c1f6e1174ef14fe
Kyra <135666155+KyrA-github@users.noreply.github.com>
2024-02-18 23:56:14 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,38 +2,41 @@ import keyboard
 import psutil
 import sys
 import time
+
 print("Start")
+bool_ = True
 
 def close_process(process_name):
     for process in psutil.process_iter(['pid', 'name']):
         if process.info['name'] == process_name:
             try:
-                # Завершаем процесс
                 psutil.Process(process.info['pid']).terminate()
                 print(f"Процесс {process_name} успешно завершен.")
             except psutil.NoSuchProcess as e:
                 print(f"Ошибка при завершении процесса {process_name}: {e}")
 
 def on_hotkey_pressed(e):
-    # Открываем файл с именем процесса
-    with open("process_name.txt",'r') as file:
-        process_name = file.read()
+    global bool_
+    file_path = 'process_name.txt'
+    data = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            data[key] = value
         file.close()
-    # Проверяем, нажата ли горячая клавиша
-    if keyboard.is_pressed('ctrl+alt+p'):
-        # Закрываем процесс "cs2"
-        close_process(process_name)
-        # Здесь можете добавить другие действия после завершения процесса
+    if keyboard.is_pressed(data.get('key')):
+        close_process(data.get('name_process'))
         print("Процесс завершен.")
+        bool_ = False
         sys.exit()
 
-# Регистрируем обработчик событий
 keyboard.hook(on_hotkey_pressed)
 
+
+
 try:
-    # Вечный цикл, чтобы ваш скрипт продолжал работать
-    while True:
+    while bool_:
         time.sleep(1)
 except KeyboardInterrupt:
-    # Обработка прерывания, если вы решите завершить скрипт
+    bool_ = False
     print("Скрипт завершен.")
